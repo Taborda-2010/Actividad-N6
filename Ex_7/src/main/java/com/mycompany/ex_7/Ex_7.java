541ac8d2ea888b7566e821e7a737320674ef1c93
Taborda-2010 <125746049+Taborda-2010@users.noreply.github.com>
2023-04-17 23:18:54 -0500

    Commit1

---
+++ Ex_7/src/main/java/com/mycompany/ex_7/Ex_7.java
@@ -0,0 +1,9 @@
+package com.mycompany.ex_7;
+public class Ex_7 {
+
+    public static void main(String[] args) {
+        VentanaPpal ventana = new VentanaPpal();
+        ventana.setVisible(true);
+        
+    }
+}
---
+++ Ex_7/src/main/java/com/mycompany/ex_7/Valores.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ex_7;
+
+/**
+ *
+ * @author pipet
+ */
+public class Valores {
+    public static String probar(double a, double b){
+        if (a>b){
+            return "A es mayor que B"; 
+        }
+        
+        else if (a<b){
+            return "A es menor que B";
+        }
+        else{
+            return "A es igual que B";
+        }
+        
+        
+        
+    }
+    
+}
