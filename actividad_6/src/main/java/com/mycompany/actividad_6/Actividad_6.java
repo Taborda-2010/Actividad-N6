33c0a6e60d03317ce24b5c01f959d33ad03be0da
Taborda-2010 <125746049+Taborda-2010@users.noreply.github.com>
2023-06-29 10:56:34 -0500

    Commit1-Ex_11.-2

---
+++ actividad_6/src/main/java/com/mycompany/actividad_6/Actividad_6.java
@@ -0,0 +1,9 @@
+package com.mycompany.actividad_6;
+public class Actividad_6 {
+
+    public static void main(String[] args) {
+         ventana iu = new ventana();
+         iu.setVisible(true);
+         iu.setLocationRelativeTo(null);  
+    }
+}
